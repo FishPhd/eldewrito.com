454116c1d3daf6faa5a1754ee0eb4f2a36b5e562
Sam Fish <samuel.fish@live.com>
2015-12-30 18:19:28 -0800

    fixes

--- js/masterserver.js
+++ js/masterserver.js
@@ -79,7 +79,7 @@ function queryServer(serverIP) {
                 url: 'https://freegeoip.net/json/' + serverIP.split(':')[0],
                 dataType: 'json',
                 jsonp: false,
-                timeout: 3000,
+                timeout: 3500,
                 success: function (geoloc) {
                     addServer(serverIP, isPassworded, serverInfo.name, serverInfo.hostPlayer, serverInfo.map, serverInfo.mapFile, serverInfo.variant, serverInfo.status, serverInfo.numPlayers, serverInfo.maxPlayers, serverInfo.eldewritoVersion, timeTaken, geoloc);
                     console.log(serverInfo);
@@ -185,5 +185,5 @@ function addServer(ip, isPassworded, name, host, map, mapfile, gamemode, status,
 }
  
 function masterserverLoop() {
-    setInterval(updateServerList, 2000);  
+    setInterval(updateServerList, 3000);  
 }
