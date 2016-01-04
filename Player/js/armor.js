696062c06fce9f07b3edf43dfec848736ecf142b
theTwister <lee.foulger.94@gmail.com>
2016-01-04 02:52:21 +0000

    Early player customizer
    armor.js
    Unfinished!

---
+++ Player/js/armor.js
@@ -0,0 +1,61 @@
+//Coded by DARKC0DE
+var dewRcon;
+var dewRconConnected = false;
+jQuery(function() {
+    StartRconConnection();
+    if (!dewRconConnected) {
+        setTimeout(StartRconConnection, 2000);
+    }
+});
+StartRconConnection = function() {
+    dewRcon = new dewRconHelper();
+    dewRcon.dewWebSocket.onopen = function() {
+        jQuery("#connectionStatus").text('Connected');
+        dewRconConnected = true;
+        connectionTrigger();
+    };
+    dewRcon.dewWebSocket.onerror = function() {
+        jQuery("#connectionStatus").text('Not connected. Is the game running?');
+        dewRconConnected = false;
+        if (!dewRconConnected) {
+            if (DewRconPortIndex == 0) {
+                DewRconPortIndex = 1;
+                StartRconConnection();
+            } else {
+                DewRconPortIndex = 0;
+                setTimeout(StartRconConnection, 1000);
+            }
+        }
+    };
+    dewRcon.dewWebSocket.onmessage = function(message) {
+        dewRcon.lastMessage = message.data;
+        console.log(dewRcon.lastMessage);
+        console.log(dewRcon.lastCommand);
+        console.log(message.data);
+    };
+    dewRcon.dewWebSocket.onclose = function(message) {
+        //jQuery("#connectionStatus").text('Disconnected');
+        //console.log(message.code);
+        dewRconConnected = false;
+        disconnectTrigger();
+    };
+}
+var DewRconPortIndex = 0;
+var DewRconPorts = [11764, 11776];
+dewRconHelper = function() {
+    window.WebSocket = window.WebSocket || window.MozWebSocket;
+    this.dewWebSocket = new WebSocket('ws://127.0.0.1:' + DewRconPorts[DewRconPortIndex], 'dew-rcon');
+    this.lastMessage = "";
+    this.lastCommand = "";
+    this.open = false;
+
+    this.send = function(command) {
+        try {
+            this.dewWebSocket.send(command);
+            this.lastCommand = command;
+        } catch (e) {
+            console.log(e);
+            dewRconConnected = false;
+        }
+    }
+}
