87e1ab667ea024a854f51bdea70e5343acd77756
FishPhd <samuel.fish@live.com>
2016-01-04 17:00:28 -0800

    slow it down

--- js/masterserver.js
+++ js/masterserver.js
@@ -281,5 +281,5 @@ function addServer(ip, isPassworded, name, host, map, mapfile, gamemode, status,
 }
  
 function masterserverLoop() {
-    setInterval(updateServerList, 2500);  
+    setInterval(updateServerList, 10000);
 }
